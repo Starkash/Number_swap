29ef27205fdf29802798937b67c13126a36b6a06
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-31 19:04:24 +0530

    Add project files.

---
+++ Number_swap/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Number_swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter First Number:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Second Number:");
+
+            int n2 = int.Parse(Console.ReadLine());
+
+            n1 = n1 + n2;
+            n2 = n1 - n2;
+            n1 = n1 - n2;
+            Console.WriteLine("Swaping of Entered Number:" +n1+", " +n2);
+
+        }
+    }
+}
